c392a93e5c61f66b7ef2f8552a6c995d74b7c903
vereedv <edvinas.veretinskas@gmail.com>
2017-07-03 22:06:38 +0100

    Fixing permissions for ansible installation script

--- roles/openshift-install/files/install-openshift.sh
+++ roles/openshift-install/files/install-openshift.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_HOST_KEY_CHECKING=False
 /usr/local/bin/ansible-playbook -i ~{{amazon_user}}/openshift_inventory.cfg ~{{amazon_user}}/openshift-ansible/playbooks/byo/config.yml
