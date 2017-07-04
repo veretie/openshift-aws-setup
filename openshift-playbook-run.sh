de047f62b1a1aa3159f1db80a926f43d9cea2700
veretie <edvinas.veretinskas@gmail.com>
2017-07-04 21:03:51 +0100

    replacing vault with AWS credentials asked by script

--- openshift-playbook-run.sh
+++ openshift-playbook-run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export ANSIBLE_HOST_KEY_CHECKING=False
-export AWS_ACCESS_KEY_ID='$1'
-export AWS_SECRET_ACCESS_KEY='$2'
-ansible-playbook install-openshift-playbook.yml -i inventory/inventory.cfg
+export AWS_ACCESS_KEY_ID=$1
+export AWS_SECRET_ACCESS_KEY=$2
+ansible-playbook openshift-playbook.yml -i inventory/inventory.cfg
