28717fab5cf58f56af7fbc0e6a1a00e52da1301c
vereedv <edvinas.veretinskas@gmail.com>
2017-07-04 21:08:38 +0100

    use script with credentials  rather then vault

--- openshift-playbook-run.sh
+++ openshift-playbook-run.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+if [ "$#" -ne 2 ]
+then
+  echo "Provide parameters: AWS access key [1] and AWS secret [2]"
+  exit 1
+fi
+
 export ANSIBLE_HOST_KEY_CHECKING=False
 export AWS_ACCESS_KEY_ID=$1
 export AWS_SECRET_ACCESS_KEY=$2
+
 ansible-playbook openshift-playbook.yml -i inventory/inventory.cfg
